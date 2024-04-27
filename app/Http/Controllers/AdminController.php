7d8663b5152cb0457120073adf7daa735422bebb
danielpsrb <danielpsrb01@gmail.com>
2024-04-27 15:28:35 +0700

    add admin controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    function index()
+    {
+        echo 'Halo, Selamat Datang Admin';
+    }
+}
