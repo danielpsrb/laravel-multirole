c4d24228df2acad169d75e0461107108f986e1b9
danielpsrb <danielpsrb01@gmail.com>
2024-04-27 14:57:42 +0700

    uodate sesi controller utk validasi email dan password

--- app/Http/Controllers/SesiController.php
+++ app/Http/Controllers/SesiController.php
@@ -10,4 +10,15 @@ class SesiController extends Controller
     {
         return view('pages.auth-login');
     }
+
+    function login(Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ], [
+            'email.required' => 'Email harus diisi',
+            'email.email' => 'Email tidak valid',
+            'password.required' => 'Password harus diisi',
+        ]);
+    }
 }
