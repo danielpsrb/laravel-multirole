1e6852858aef6a5719ad963d73572e05ab8f18bc
danielpsrb <danielpsrb01@gmail.com>
2024-04-27 14:51:18 +0700

    add login pgae

---
+++ resources/views/pages/auth-login.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container py-5">
+        <div class="w-50 center border rounded px-3 py-3 mx-auto">
+        <h1>Login</h1>
+        <form action="" method="POST">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" value="" name="email" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" class="form-control">
+            </div>
+            <div class="mb-3 d-grid">
+                <button name="submit" type="submit" class="btn btn-primary">Login</button>
+            </div>
+        </form>
+    </div>
+    </div>
+</body>
+</html>
