959e2f166036978ab86402e2e5bde34c5c999120
danielpsrb <danielpsrb01@gmail.com>
2024-04-27 15:32:52 +0700

    update route for admin page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\SesiController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SesiController::class, 'index']);
 Route::post('/', [SesiController::class, 'login']);
+
+Route::get('/admin', [AdminController::class, 'index']);
