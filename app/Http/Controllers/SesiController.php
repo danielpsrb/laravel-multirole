fd25e04ec7a3a11db0318637d0f331096af2170c
danielpsrb <danielpsrb01@gmail.com>
2024-04-27 14:42:47 +0700

    add sesi controller

---
+++ app/Http/Controllers/SesiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SesiController extends Controller
+{
+    function index()
+    {
+        echo "Hello World!";
+    }
+}
