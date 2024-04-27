ac47be2297a6da91920a18278de737d1810d5831
danielpsrb <danielpsrb01@gmail.com>
2024-04-27 15:01:03 +0700

    menambahkan csrf pada halaman login

--- resources/views/pages/auth-login.blade.php
+++ resources/views/pages/auth-login.blade.php
@@ -13,6 +13,7 @@
         <div class="w-50 center border rounded px-3 py-3 mx-auto">
         <h1>Login</h1>
         <form action="" method="POST">
+            @csrf
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input type="email" value="" name="email" class="form-control">
