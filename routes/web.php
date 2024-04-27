3920a646c9fb07cd80ed0c7286a7e64a189ed20a
danielpsrb <danielpsrb01@gmail.com>
2024-04-27 16:33:36 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\SesiController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [SesiController::class, 'index']);
+    Route::post('/', [SesiController::class, 'login']);
+});
 
-Route::get('/', [SesiController::class, 'index']);
-Route::post('/', [SesiController::class, 'login']);
+Route::get('/home', function () {
+    return redirect('/admin');
+});
 
 Route::get('/admin', [AdminController::class, 'index']);
