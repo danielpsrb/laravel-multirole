aa00df98a5dcccb716ab90d7d7948ab2c4d0b219
danielpsrb <danielpsrb01@gmail.com>
2024-04-27 17:03:38 +0700

    add middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\Admin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
