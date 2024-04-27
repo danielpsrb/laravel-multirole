6142392e8456a3e280a792a2b65a3618c55d8302
danielpsrb <danielpsrb01@gmail.com>
2024-04-27 14:48:59 +0700

    upade route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\SesiController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [SesiController::class, 'index']);
