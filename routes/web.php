eee56af07cc7f48af44ecd60a6b6193ce10b090f
danielpsrb <danielpsrb01@gmail.com>
2024-04-27 18:34:30 +0700

    update route dengan memasukkan masing masing middleware

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,28 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\SesiController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\RedirectIfAuthenticated;
+use App\Http\Middleware\UsersAccess;
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/', [SesiController::class, 'index']);
-    Route::post('/', [SesiController::class, 'login']);
-});
+
+// Route::middleware(['auth'])->group(function () {
+//     Route::get('/', [SesiController::class, 'index']);
+//     Route::post('/', [SesiController::class, 'login']);
+// });
+
+Route::get('/', [SesiController::class, 'index'])->middleware(RedirectIfAuthenticated::class)->name('login');
+Route::post('/', [SesiController::class, 'login']);
 
 Route::get('/home', function () {
-    return redirect('/admin');
+    return redirect('/users');
 });
 
-Route::get('/admin', [AdminController::class, 'index']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('/users', [AdminController::class, 'index']);
+    Route::get('/admin', [AdminController::class, 'admin'])->middleware(UsersAccess::class . ':admin');
+    Route::get('/staff-admin', [AdminController::class, 'staffAdmin'])->middleware(UsersAccess::class . ':staff admin');
+    Route::get('/logout', [SesiController::class, 'logout']);
+});
+
+// Route::get('/admin', [AdminController::class, 'index']);
+// Route::get('/logout', [SesiController::class, 'logout']);
