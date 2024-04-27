ceb46187dc48785b8e50aa8aac25898629f9d419
danielpsrb <danielpsrb01@gmail.com>
2024-04-27 15:20:01 +0700

    update sesi controller untuk membuat validasi email password salah

--- app/Http/Controllers/SesiController.php
+++ app/Http/Controllers/SesiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SesiController extends Controller
 {
@@ -20,5 +21,16 @@ class SesiController extends Controller
             'email.email' => 'Email tidak valid',
             'password.required' => 'Password harus diisi',
         ]);
+
+        $loginInfo = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if(Auth::attempt($loginInfo)) {
+            echo 'Login berhasil';exit();
+        } else {
+            return redirect('')->withErrors('Email atau password salah')->withInput();
+        }
     }
 }
