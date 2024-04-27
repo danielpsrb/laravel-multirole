a681020714fa6c3059603d05522be750af6645dc
danielpsrb <danielpsrb01@gmail.com>
2024-04-27 15:40:44 +0700

    update admin controller untuk menampilkan nama pengguna

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     function index()
     {
         echo 'Halo, Selamat Datang Admin';
+        echo "<h1>". Auth::user()->name."</h1>";
     }
 }
